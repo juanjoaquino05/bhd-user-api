778c735d2682e5a4ffdaab4838331e6fbf64d797
Juan Henriquez <juanjoaquino05@gmail.com>
2025-03-23 12:52:39 -0400

    Springboot initialization (with some libs)

---
+++ src/main/java/com/bhd/user_api/UserApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bhd.user_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApiServiceApplication.class, args);
+	}
+
+}
