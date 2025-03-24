2cb352061a9b0d9aa9e28ef6e0200ed03b1d2bed
Juan Henriquez <juanjoaquino05@gmail.com>
2025-03-24 00:50:17 -0400

    Fixing validations

--- src/main/java/com/bhd/user_api/user/dto/request/CreateUserRequest.java
+++ src/main/java/com/bhd/user_api/user/dto/request/CreateUserRequest.java
@@ -3,6 +3,8 @@ package com.bhd.user_api.user.dto.request;
 import com.bhd.user_api.user.entity.Phone;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 import java.util.List;
@@ -10,16 +12,20 @@ import java.util.List;
 @Getter
 public class CreateUserRequest {
 
+    @NotEmpty(message = "Nombre no puede ser vacio")
     @JsonProperty(value = "name", required = true)
     private String name;
 
-    @Email(message = "Invalid email")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "Correo invalido")
+    @NotEmpty(message = "Email no puede ser vacio")
     @JsonProperty(value = "email", required = true)
     private String email;
 
+    @NotEmpty(message = "Clave no puede ser vacio")
     @JsonProperty(value = "password", required = true)
     private String password;
 
+    @NotEmpty(message = "Telefonos no puede ser vacio")
     @JsonProperty(value = "phones", required = true)
     private List<Phone> phones;
 }
